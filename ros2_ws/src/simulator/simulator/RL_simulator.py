4023e4ee5fdd388548bd1510b1b5ae190574733a
Alexandra <alex.f.s.johansson@gmail.com>
2025-06-03 14:56:48 +0200

    Fixed it so the ROS nodes use the viritual python environment

--- ros2_ws/src/simulator/simulator/RL_simulator.py
+++ ros2_ws/src/simulator/simulator/RL_simulator.py
@@ -1,12 +1,19 @@
+#!/home/alex/Desktop/Exjobb/RL_collision_avoidance/RL_collision_avoidance_env/bin/python
+
+import sys
+print("Python executable:", sys.executable)
+import os
+
 import numpy as np
+
 import pybullet as p
 from gymnasium import spaces
-
+'''
 from gym_pybullet_drones.envs.BaseRLAviary import BaseRLAviary
 from gym_pybullet_drones.utils.enums import DroneModel, Physics, ActionType, ObservationType, ImageType
 from gym_pybullet_drones.control.DSLPIDControl import DSLPIDControl
 
-
+'''
 def main():
     print('Hi from simulator.')
 
