ae1a94b8dc66664b3f29d1dd93b84e60504b0790
Alexandra <alex.f.s.johansson@gmail.com>
2025-06-09 10:59:58 +0200

    Added more parameters for testing

--- Training/code/test.py
+++ Training/code/test.py
@@ -8,13 +8,27 @@ from RLEnv import RLEnv
 
 parameters = {
     'drone_model': DroneModel.CF2X,
-    'initial_xyzs': np.array([[1, 0, 1]]),
+    'num_objects': 1,  # Number of balls to add
+    'initial_xyzs': np.array([[1, 2, 0.1]]),
     'ctrl_freq': 240,
+    'target_pos': np.array([[.0, .0, 1.0]]),
+    'target_radius': 0.25,  # Radius of the target sphere
+    'avoidance_radius': 0.75,  # Radius of the avoidance sphere
+    'episode_length': 60,  # seconds
+    'act4': True, # Use actions x,y,z and velocity or only position
+    'obs_noise': False,  # Add noise to the observations
+    'obs_noise_std': 0.01,  # Standard deviation of the noise
+    'reward_collision': 200.0,
+    'reward_terminated': 100.0,
+    'reward_target_distance_delta': 10.0,
+    'reward_object_distance_delta': 10.0,
+    'reward_step': 0.01,
+    'reward_in_target': 1.0
 }
 
 env = RLEnv(parameters=parameters,gui=True)
 
-action = np.array([[0.0, 0.0, 1.0]])
+action = np.array([[.0, .0, 1.0]])
 
 for i in range (10000):
         '''
@@ -23,14 +37,17 @@ for i in range (10000):
         action[0,0] = x
         action[0,1] = y
         '''
-        
-        if (i%100 == 0):
+        if (i%1000 == 0):
             # Add a ball at a random position
-            #env.addBallRandom()
+            env.addBallRandom()
 
             # Add a ball at a fixed position for collision testing
             #env.addBall([0, 0, 0.5], [0, 0, 100])
+
+            #env.reset()
+            pass
         
 
-        env.step(action)
+        obs, reward, terminated, truncated, info = env.step(action)
+        #print("Obs:", obs, "\tAction", action, "\tReward:", reward, "\tTerminated:", terminated, "\tTruncated:", truncated, "\tInfo:", info)
         time.sleep(1./240.)
