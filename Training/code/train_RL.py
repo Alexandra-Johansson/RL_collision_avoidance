a167d45c61356d47699a74f6395bb26216339373
Alexandra <alex.f.s.johansson@gmail.com>
2025-06-10 16:26:20 +0200

    Change some parameters for testing

--- Training/code/train_RL.py
+++ Training/code/train_RL.py
@@ -13,22 +13,23 @@ parameters = {
     'target_pos': np.array([[.0, .0, 1.0]]),
     'target_radius': 0.25,  # Radius of the target sphere
     'avoidance_radius': 0.75,  # Radius of the avoidance sphere
-    'episode_length': 5,  # seconds
+    'episode_length': 30,  # seconds
     'eval_episodes' : 1,  # Number of episodes to evaluate
     'learning_rate': 0.001,
     'batch_size': 8,
-    'train_freq': 1,  # Train every step
+    'train_freq': 4,  # Train every step
     'gradient_steps': -1,  # Number of gradient steps per update
     'obs_noise': False,  # Add noise to the observations
     'obs_noise_std': 0.01,  # Standard deviation of the noise
-    'reward_collision': 200.0,
+    'reward_collision': -200.0,
     'reward_terminated': 1000.0,
-    'reward_target_distance_delta': 1.0,
-    'reward_object_distance_delta': 10.0,
-    'reward_step': 0.01,
-    'reward_in_target': 10.0,
+    'reward_target_distance': -10.0,
+    'reward_target_distance_delta': 10.0, # Positive for rewarding moving towards target
+    'reward_object_distance_delta': -10.0, # Negative for rewarding moving away from target
+    'reward_step': -1,
+    'reward_in_target': 100.0,
     'target_reward': 15000.0,  # Reward to stop training
-    'total_timesteps': int(3e6),  # Total timesteps to train
+    'total_timesteps': int(2e4),  # Total timesteps to train
     'nr_of_env': 1,  # Number of environments to train in parallel
 }
 
