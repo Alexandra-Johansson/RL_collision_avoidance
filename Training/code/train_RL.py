986d067bc57e328e9aa8c201688c7746a9b60ca7
Alexandra <alex.f.s.johansson@gmail.com>
2025-06-24 11:23:02 +0200

    Tweaked some parameters

--- Training/code/train_RL.py
+++ Training/code/train_RL.py
@@ -8,34 +8,39 @@ parameters = {
     'initial_xyzs': np.array([[1.0, 2.0, 1.0]]),
     'ctrl_freq': 60,
     'pyb_freq': 240,
-    'action_size': 0.2,
+    'action_size': 0.1,
     'eval_freq': 1,  # Evaluate every 1 episodes
     'target_pos': np.array([[.0, .0, 1.0]]),
-    'target_radius': 0.25,  # Radius of the target sphere
+    'target_radius': 0.1,  # Radius of the target sphere
     'avoidance_radius': 0.75,  # Radius of the avoidance sphere
     'episode_length': 30,  # seconds
-    'eval_episodes' : 5,  # Number of episodes to evaluate
+    'eval_episodes' : 1,  # Number of episodes to evaluate
     'learning_rate': 0.001,
-    'batch_size': 256,
-    'train_freq': 4,  # Train every step
+    'batch_size': 64,
+    'num_epochs': 20,
+    'train_freq': 1,  # Train every step
     'gradient_steps': -1,  # Number of gradient steps per update
     'obs_noise': False,  # Add noise to the observations
     'obs_noise_std': 0.01,  # Standard deviation of the noise
-    'reward_collision': -500.0,
+    'reward_collision': -200.0,
     'reward_terminated': 1000.0,
     'reward_target_distance': -10.0,
     'reward_target_distance_delta': 100.0, # Positive for rewarding moving towards target
     'reward_angular_velocity_delta': -10.0, # Negative reward for changing angle
     'reward_object_distance_delta': -10.0, # Negative for rewarding moving away from target
-    'reward_step': -1,
+    'reward_step': -5,
     'reward_in_target': 100.0,
     'target_reward': 150000.0,  # Reward to stop training
     'total_timesteps': int(2e6),  # Total timesteps to train
-    'nr_of_env': 1,  # Number of environments to train in parallel
+    'nr_of_env': 10,  # Number of environments to train in parallel
 }
 
-PPO = Train_PPO(parameters=parameters, train_gui=False)
+if __name__ == "__main__":
+    test_params_learning_rate = [3*1e-5,2*1e-4,1e-3]
 
-PPO.train()
+    for learning_rate in test_params_learning_rate:
+        parameters['learning_rate'] = learning_rate
+        
+        PPO = Train_PPO(parameters=parameters, train_gui=False)
 
-#PPO.train()
+        PPO.train()
