8b2dce22fe30e6fe4ae493704c549698c23c1885
Alexandra <alex.f.s.johansson@gmail.com>
2025-06-27 13:31:10 +0200

    Minor fixes, now returns time taken to train model

--- Training/code/train_PPO.py
+++ Training/code/train_PPO.py
@@ -2,6 +2,7 @@ import math
 import os
 import time
 from datetime import datetime
+import traceback
 
 import numpy as np
 from data_handling import Txt_file, Plot
@@ -13,35 +14,33 @@ from RLEnv import RLEnv
 from data_handling import Plot
 
 from stable_baselines3 import PPO
-from stable_baselines3.common.buffers import DictReplayBuffer
 from stable_baselines3.common.callbacks import (
     EvalCallback,
     StopTrainingOnMaxEpisodes,
     StopTrainingOnRewardThreshold,
 )
 from stable_baselines3.common.env_util import make_vec_env
-from stable_baselines3.common.vec_env import SubprocVecEnv
+from stable_baselines3.common.vec_env import SubprocVecEnv, DummyVecEnv
 from stable_baselines3.common.evaluation import evaluate_policy
 from stable_baselines3.common.monitor import Monitor
-from stable_baselines3.common.noise import (
-    NormalActionNoise,
-    OrnsteinUhlenbeckActionNoise,
-)
 
 
 class Train_PPO():
     def __init__(self, parameters, train_gui : bool = False):
         self.parameters = parameters
 
-        self.eval_freq = self.parameters['eval_freq']*self.parameters['ctrl_freq']*self.parameters['episode_length']
+        if (self.parameters['num_steps']*self.parameters['nr_of_env'] % self.parameters['batch_size']):
+            raise Exception("N_steps*n_envs not divisiable by batch_size")
+
+        self.eval_freq = self.parameters['eval_freq']*self.parameters['num_steps']*self.parameters['nr_of_env']
 
         self.train_gui = train_gui
 
         self.output_folder = 'Training/results'
 
-        self.filename = os.path.join(self.output_folder, 'PPO_training_' + datetime.now().strftime("%d.%m.%Y-%H.%M.%S"))
+        self.filename = os.path.join(self.output_folder, 'PPO_training_' + datetime.now().strftime("%Y.%m.%d-%H.%M.%S"))
         if not os.path.exists(self.filename):
-            os.makedirs(self.filename + '/')
+            os.makedirs(self.filename)
 
         self.plot = Plot(self.filename)
 
@@ -56,10 +55,12 @@ class Train_PPO():
                                     seed = 0, 
                                     vec_env_cls=SubprocVecEnv)
 
+        
         eval_env = SubprocVecEnv([
                                     lambda: Monitor(RLEnv(parameters=self.parameters, gui=self.train_gui))
                                     for _ in range(self.parameters['eval_episodes'])
-])
+                                ])
+        #eval_env = DummyVecEnv([lambda: Monitor(RLEnv(parameters=self.parameters, gui=self.train_gui))])
 
         print("[INFO] Action space: ", training_env.action_space)
         print("[INFO] Observation space: ", training_env.observation_space)
@@ -81,17 +82,21 @@ class Train_PPO():
                                      n_eval_episodes = self.parameters['eval_episodes'],
                                      best_model_save_path = self.filename,
                                      log_path = self.filename + '/logs/',
-                                     eval_freq = max(self.eval_freq // self.parameters['nr_of_env'], 1),
+                                     eval_freq = self.eval_freq,
                                      deterministic = True)
 
         self.plot.tensorboard()
 
         time_start = time.time()
-        model.learn(total_timesteps=self.parameters['total_timesteps'],
-                    callback=eval_callback,
-                    log_interval = self.parameters['nr_of_env'],
-                    progress_bar = True)
-        
+        try:
+            model.learn(total_timesteps=self.parameters['total_timesteps'],
+                        callback=eval_callback,
+                        log_interval = 1,
+                        progress_bar = True)
+        except Exception as e:
+            print("Exception occured: ", e)
+            traceback.print_exc()
+            i = 1
         time_end = time.time()
 
         model.save(self.filename+'/final_model.zip')
@@ -104,10 +109,11 @@ class Train_PPO():
         time_taken_m = math.floor(time_taken/60)
         time_taken_s = round(time_taken - time_taken_m*60)
 
-        print(f"Model trained in: {time_taken_h}h.{time_taken_m}m.{time_taken_s}s.")
+        time_taken_str = f"Model trained in: {time_taken_h}h.{time_taken_m}m.{time_taken_s}s."
+
+        print(time_taken_str)
 
         eval_env.close()
         training_env.close()
 
-    def make_eval_env(self):
-        return Monitor(RLEnv(parameters=self.parameters, gui=self.train_gui))
+        return time_taken_str
