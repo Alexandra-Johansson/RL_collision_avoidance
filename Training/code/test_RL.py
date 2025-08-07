c751e8956071acff70347836a7dc65086df6866e
Alexandra Johansson <alejo009@student.liu.se>
2025-08-07 08:49:34 +0200

    Modified test file slightly

--- Training/code/test_RL.py
+++ Training/code/test_RL.py
@@ -8,15 +8,18 @@ from stable_baselines3 import PPO
 from stable_baselines3.common.evaluation import evaluate_policy
 from stable_baselines3.common.monitor import Monitor
 
-folder = "Training/results/PPO_training_2025.07.04-23.29.23"  # Path to the folder with the trained model
+folder = "Training/results/PPO_training_2025.08.05-23.12.00"  # Path to the folder with the trained model
 
 param_file = Txt_file(folder)
 parameters = param_file.load_parameters()
 
-model = PPO.load(folder + "/best_model.zip")
+model = PPO.load(folder + "/final_model.zip")
 print("Model loaded")
 
-test_env = Monitor(RLEnv(parameters = parameters, gui = True))
+input("Press enter to continue...")
+
+parameters['gui'] = True
+test_env = Monitor(RLEnv(parameters = parameters))
 
 mean_reward, std_reward = evaluate_policy(model,
                                           test_env,
