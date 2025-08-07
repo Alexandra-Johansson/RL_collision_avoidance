c0a1431b29065db5b54cc860081e1f0a852d1314
Alexandra Johansson <alejo009@student.liu.se>
2025-08-07 08:48:54 +0200

    Added training using DDPG

---
+++ Training/code/train_DDPG.py
@@ -0,0 +1,277 @@
+import math
+import os
+import time
+import traceback
+from datetime import datetime
+
+import numpy as np
+from data_handling import Plot, Txt_file
+from RLEnv import RLEnv
+from stable_baselines3 import DDPG
+from stable_baselines3.common.callbacks import (
+    BaseCallback,
+    CallbackList,
+    EvalCallback,
+    StopTrainingOnRewardThreshold,
+)
+from stable_baselines3.common.env_checker import check_env
+from stable_baselines3.common.env_util import make_vec_env
+from stable_baselines3.common.monitor import Monitor
+from stable_baselines3.common.noise import NormalActionNoise
+from stable_baselines3.common.vec_env import SubprocVecEnv, VecNormalize
+
+
+class CustomTensorboardCallback(BaseCallback):
+    def __init__(self, verbose=0):
+        super().__init__(verbose)
+        self.episode_infos = []
+
+    def _on_step(self) -> bool:
+        # Check if an episode ended
+        infos = self.locals.get("infos", [])
+        dones = self.locals.get("dones", [])
+        for info, done in zip(infos,dones):
+            if done and info is not None:
+                self.episode_infos.append(info)
+                '''
+                if "min_object_distance" in info:
+                    self.logger.record("custom/min_object_distance", info["min_object_distance"])
+                if "max_target_distance" in info:
+                    self.logger.record("custom/max_target_distance", info["max_target_distance"])
+                if "final_drone_altitude" in info:
+                    self.logger.record("custom/final_drone_altitude", info["final_drone_altitude"])
+                if "final_target_distance" in info:
+                    self.logger.record("custom/final_target_distance", info["final_target_distance"])
+                if "out_of_bounds" in info:
+                    self.logger.record("custom/out_of_bounds", info["out_of_bounds"])
+                if "orientation_out_of_bounds" in info:
+                    self.logger.record("custom/orientation_out_of_bounds", info["orientation_out_of_bounds"])
+                if "time_limit_reached" in info:
+                    self.logger.record("custom/time_limit_reached", info["time_limit_reached"])
+                if "obj_collision" in info:
+                    self.logger.record("custom/obj_collision", info["obj_collision"])
+                if "contact_collision" in info:
+                    self.logger.record("custom/contact_collision", info["contact_collision"])
+                '''
+        '''
+        for info in infos:
+            if info is not None and "kf_pos_error" in info:
+                self.logger.record("custom/kf_pos_error", info["kf_pos_error"])
+            if info is not None and "kf_vel_error" in info:
+                self.logger.record("custom/kf_vel_error", info["kf_vel_error"])
+        '''
+        return True
+
+    def _on_rollout_end(self):
+        # Log all episode infos collected during the rollout
+        min_obj_distances = [
+            info["min_object_distance"]
+            for info in self.episode_infos
+            if info is not None and "min_object_distance" in info
+        ]
+        if min_obj_distances:
+            mean_min_obj_distance = np.mean(min_obj_distances)
+            self.logger.record("custom/mean_min_object_distance", mean_min_obj_distance)
+
+        max_target_distances = [
+            info["max_target_distance"]
+            for info in self.episode_infos
+            if info is not None and "max_target_distance" in info
+        ]
+        if max_target_distances:
+            mean_max_target_distance = np.mean(max_target_distances)
+            self.logger.record("custom/mean_max_target_distance", mean_max_target_distance)
+
+        final_drone_altitudes = [
+            info["final_drone_altitude"]
+            for info in self.episode_infos
+            if info is not None and "final_drone_altitude" in info
+        ]
+        if final_drone_altitudes:
+            mean_final_drone_altitude = np.mean(final_drone_altitudes)
+            self.logger.record("custom/mean_final_drone_altitude", mean_final_drone_altitude)
+
+        final_target_distances = [
+            info["final_target_distance"]
+            for info in self.episode_infos
+            if info is not None and "final_target_distance" in info
+        ]
+        if final_target_distances:
+            mean_final_target_distance = np.mean(final_target_distances)
+            self.logger.record("custom/mean_final_target_distance", mean_final_target_distance)
+
+        out_of_bounds = [
+            info["out_of_bounds"]
+            for info in self.episode_infos
+            if info is not None and "out_of_bounds" in info
+        ]
+        if out_of_bounds:
+            mean_out_of_bounds = np.mean([int(ob) for ob in out_of_bounds])
+            self.logger.record("custom/mean_out_of_bounds", mean_out_of_bounds)
+
+        orientation_out_of_bounds = [
+            info["orientation_out_of_bounds"]
+            for info in self.episode_infos
+            if info is not None and "orientation_out_of_bounds" in info
+        ]
+        if orientation_out_of_bounds:
+            mean_orientation_out_of_bounds = np.mean([int(oob) for oob in orientation_out_of_bounds])
+            self.logger.record("custom/mean_orientation_out_of_bounds", mean_orientation_out_of_bounds)
+
+        time_limit_reached = [
+            info["time_limit_reached"]
+            for info in self.episode_infos
+            if info is not None and "time_limit_reached" in info
+        ]
+        if time_limit_reached:
+            mean_time_limit_reached = np.mean([int(tlr) for tlr in time_limit_reached])
+            self.logger.record("custom/mean_time_limit_reached", mean_time_limit_reached)
+
+        object_collisions = [
+            info["obj_collision"]
+            for info in self.episode_infos
+            if info is not None and "obj_collision" in info
+        ]
+        if object_collisions:
+            mean_object_collision = np.mean([int(oc) for oc in object_collisions])
+            self.logger.record("custom/mean_object_collision", mean_object_collision)
+
+        contact_collisions = [
+            info["contact_collision"]
+            for info in self.episode_infos
+            if info is not None and "contact_collision" in info
+        ]
+        if contact_collisions:
+            mean_contact_collision = np.mean([int(cc) for cc in contact_collisions])
+            self.logger.record("custom/mean_contact_collision", mean_contact_collision)
+
+        max_kf_pos_errors = [
+            info["max_kf_pos_error"]
+            for info in self.episode_infos
+            if info is not None and "max_kf_pos_error" in info
+        ]
+        if max_kf_pos_errors:
+            mean_max_kf_pos_error = np.mean(max_kf_pos_errors)
+            self.logger.record("custom/mean_max_kf_pos_error", mean_max_kf_pos_error)
+
+        max_kf_vel_errors = [
+            info["average_kf_vel_error"]
+            for info in self.episode_infos
+            if info is not None and "average_kf_vel_error" in info
+        ]
+        if max_kf_vel_errors:
+            mean_max_kf_vel_error = np.mean(max_kf_vel_errors)
+            self.logger.record("custom/mean_average_kf_vel_error", mean_max_kf_vel_error)
+        
+        self.episode_infos = [] # Resetting episode/rollout info
+
+        return True
+    
+class Train_DDPG():
+    def __init__(self, parameters):
+        self.parameters = parameters
+
+        if (self.parameters['num_steps']*self.parameters['nr_of_env'] % self.parameters['batch_size']):
+            raise Exception("N_steps*n_envs not divisiable by batch_size")
+
+        self.eval_freq = self.parameters['eval_freq']*self.parameters['num_steps']
+
+        self.output_folder = 'Training/results'
+
+        self.filename = os.path.join(self.output_folder, 'DDPG_training_' + datetime.now().strftime("%Y.%m.%d-%H.%M.%S"))
+        if not os.path.exists(self.filename):
+            os.makedirs(self.filename)
+
+        self.plot = Plot(self.filename)
+
+        Param = Txt_file(self.filename)
+        Param.save_parameters(self.parameters)
+
+    def train(self):
+        # Check environment and outputs warnings
+        test_parameters = self.parameters.copy()
+        test_parameters['gui'] = False  # Disable GUI for environment check
+        test_env = RLEnv(parameters=self.parameters)
+        check_env(test_env)
+
+        # Create the environment
+        training_env = make_vec_env(RLEnv, 
+                                    env_kwargs = dict(parameters = self.parameters), 
+                                    n_envs = self.parameters['nr_of_env'],
+                                    seed = 0, 
+                                    vec_env_cls=SubprocVecEnv)
+        
+        training_env = VecNormalize(training_env, norm_obs=True, norm_reward=True, clip_obs=10.0)
+        
+        self.parameters['gui'] = False
+        eval_env = SubprocVecEnv([
+                                    lambda: Monitor(RLEnv(parameters=self.parameters))
+                                    for _ in range(self.parameters['eval_episodes'])
+                                ])
+        
+        eval_env = VecNormalize(eval_env, norm_obs=True, norm_reward=True, clip_obs=10.0)
+        eval_env.obs_rms = training_env.obs_rms  # Use the same normalization as the training environment
+
+        print("[INFO] Action space: ", training_env.action_space)
+        print("[INFO] Observation space: ", training_env.observation_space)
+
+        n_actions = training_env.action_space.shape
+        action_noise = NormalActionNoise(mean = np.zeros(n_actions), sigma = 1/3 * np.ones(n_actions))
+
+        model = DDPG('MultiInputPolicy',
+                    training_env,
+                    learning_rate = self.parameters['learning_rate'],
+                    learning_starts = self.parameters['learning_starts'],
+                    batch_size = self.parameters['batch_size'],
+                    action_noise = action_noise,
+                    train_freq = (int(self.parameters['training_freq']), "step"), # int(eval_env.CTRL_FREQ/2)
+                    gradient_steps = self.parameters['gradient_steps'],
+                    tensorboard_log = self.filename + '/tensorboard_logs/',
+                    verbose = 1) # TODO
+
+        callback_on_best = StopTrainingOnRewardThreshold(reward_threshold = self.parameters['target_reward'],
+                                                        verbose=1)
+
+        eval_callback = EvalCallback(eval_env,
+                                     callback_on_new_best = callback_on_best,
+                                     verbose = 1,
+                                     n_eval_episodes = self.parameters['eval_episodes'],
+                                     eval_freq = self.eval_freq,
+                                     log_path = self.filename + '/logs/',
+                                     best_model_save_path = self.filename,
+                                     deterministic = True)
+
+        callback_list = CallbackList([eval_callback, CustomTensorboardCallback()])
+
+        self.plot.tensorboard()
+
+        time_start = time.time()
+        try:
+            model.learn(total_timesteps=self.parameters['total_timesteps'],
+                        callback=callback_list,
+                        log_interval = 1,
+                        progress_bar = True)
+        except Exception as e:
+            print("Exception occured: ", e)
+            traceback.print_exc()
+            input("Error occured! Press enter to continue...")
+        time_end = time.time()
+
+        model.save(self.filename+'/final_model.zip')
+        print(self.filename)
+        print("Model saved")
+
+        time_taken = time_end - time_start
+        time_taken_h = math.floor(time_taken/(60*60))
+        time_taken -= time_taken_h*60*60
+        time_taken_m = math.floor(time_taken/60)
+        time_taken_s = round(time_taken - time_taken_m*60)
+
+        time_taken_str = f"Model trained in: {time_taken_h}h.{time_taken_m}m.{time_taken_s}s."
+
+        print(time_taken_str)
+
+        eval_env.close()
+        training_env.close()
+
+        return time_taken_str
--- Training/code/train_RL.py
+++ Training/code/train_RL.py
@@ -1,8 +1,63 @@
 import numpy as np
 from gym_pybullet_drones.utils.enums import DroneModel
-from train_PPO import Train_PPO
 
-parameters = {
+rl_algorithm = "DDPG" # DDPG or PPO
+if rl_algorithm == "DDPG":
+    from train_DDPG import Train_DDPG
+
+    parameters_DDPG = {
+    'drone_model': DroneModel.CF2X,
+    'num_objects': 1,  # Number of balls to add
+    'initial_xyzs': np.array([[.0, .0, 1.0]]),
+    'ctrl_freq': 60,
+    'pyb_freq': 1*240,
+    'action_size': 0.5,
+    'target_pos': np.array([[.0, .0, 1.0]]),
+    'target_radius': 0.2,  # Radius of the target sphere
+    'avoidance_radius': 5.,  # Radius of the avoidance sphere
+    'critical_safety_distance': 0.2,
+    'episode_length': 8,  # seconds
+    'eval_freq': 20,  # Evaluate every n episodes
+    'eval_episodes' : 5,  # Number of episodes to evaluate
+    'learning_rate': 5e-4, 
+    'batch_size': 1024, # Should be a divisor of n_steps*n_envs
+    'num_steps': 1024, # Number of steps in an episode before updating policy, actual update is n_steps*n_envs
+    'nr_of_env': 4,  # Number of environments to train in parallel
+    'num_epochs': 5,
+    'entropy_coefficient': 0.0,
+    'obs_noise': False,  # Add noise to the observations
+    'obs_noise_std': 0.05,  # Standard deviation of the noise
+    'kf_process_noise': 1e-2,
+    'kf_measurement_noise': 1e-6,
+    'reward_collision': -1.0,   # Negative reward for collision
+    'reward_sucess': 1.0,   # Positive reward for avoiding collision and returning to target
+    'reward_end_outside_target': -0.5,  # Negative reward for ending outside the target
+    'reward_truncation': -1.0,  # Negative reward for truncation
+    'reward_target_distance': -0.0,
+    'reward_target_distance_delta': 0.0, # Positive for rewarding moving towards target
+    'reward_rpy': -0.0,  # Negative reward for angular velocity
+    'reward_angular_velocity_delta': -0.0, # Negative reward for changing angle
+    'reward_object_distance': -0.0,  # Negative reward for being close to the object
+    'reward_object_distance_delta': 0.0, # Positive for rewarding moving away from object
+    'reward_action_difference': -0.15,
+    'reward_step': -0,
+    'reward_in_target': 0.0,
+    'target_reward': 150000.0,  # Reward to stop training
+    'total_timesteps': int(3*1e6),  # Total timesteps to train
+    'gui': False,  # Whether to use GUI or not
+    'obs_timestep': False, # Include timestep in observation
+    'obs_obj_vel': False, # Include object velocity in observation
+    'obs_kf': False, # Include kalmanfilter estimates in observation, otherwise use PyBullet data
+    'obs_action_difference': True,  # Include action difference in observation
+    'learning_starts': 100000,
+    'training_freq': 1, # Update model every 'train_freq' steps
+    'gradient_steps': -1 # Amount of gradient steps, -1 = same as train_freq
+    }
+
+if rl_algorithm == "PPO":
+    from train_PPO import Train_PPO
+
+    parameters_PPO = {
     'drone_model': DroneModel.CF2X,
     'num_objects': 1,  # Number of balls to add
     'initial_xyzs': np.array([[.0, .0, 1.0]]),
@@ -30,75 +85,90 @@ parameters = {
     'reward_sucess': 1.0,   # Positive reward for avoiding collision and returning to target
     'reward_end_outside_target': -0.5,  # Negative reward for ending outside the target
     'reward_truncation': -1.0,  # Negative reward for truncation
-    'reward_target_distance': -0.001,
+    'reward_target_distance': -0.0,
     'reward_target_distance_delta': 0.0, # Positive for rewarding moving towards target
-    'reward_rpy': -0.01,  # Negative reward for angular velocity
-    'reward_angular_velocity_delta': -5.0, # Negative reward for changing angle
-    'reward_object_distance': -0.001,  # Negative reward for being close to the object
-    'reward_object_distance_delta': 10.0, # Positive for rewarding moving away from object
-    'reward_step': -100,
-    'reward_in_target': 200.0,
+    'reward_rpy': -0.0,  # Negative reward for angular velocity
+    'reward_angular_velocity_delta': -0.0, # Negative reward for changing angle
+    'reward_object_distance': -0.0,  # Negative reward for being close to the object
+    'reward_object_distance_delta': 0.0, # Positive for rewarding moving away from object
+    'reward_step': -0,
+    'reward_in_target': 0.0,
     'target_reward': 150000.0,  # Reward to stop training
-    'total_timesteps': int(3*1e6),  # Total timesteps to train
+    'total_timesteps': int(6*1e6),  # Total timesteps to train
     'gui': False,  # Whether to use GUI or not
     'obs_timestep': False, # Include timestep in observation
     'obs_obj_vel': True, # Include object velocity in observation
     'obs_kf': False, # Include kalmanfilter estimates in observation, otherwise use PyBullet data
     'obs_action_difference': True,  # Include action difference in observation
     'clip_range': 0.2
-}
+    }
 
 if __name__ == "__main__":
 
     time_taken = []
 
-    test_param_learning_rate = [7e-4]
-    test_param_reward_target_distance = [-0.1]
-    test_param_reward_object_distance_delta = [0.1]
-    test_param_reward_in_target = [0.2]
-    test_param_reward_object_distance = [0.15]
-    test_param_reward_rpy = [-0.15]
-    test_param_obs_timestep = [False]
-    test_param_obs_action_difference = [False]
-    test_param_entropy_coefficent = [0, 10]
-    test_param_reward_truncation = [-1.0, -0.5]
-    test_param_clip_range = [0.3, 0.5]
+    if rl_algorithm == "PPO":
+        test_param_learning_rate = [8e-4]
+        test_param_reward_target_distance = [-0.0]
+        test_param_reward_object_distance_delta = [0.0]
+        test_param_reward_in_target = [0.0]
+        test_param_reward_object_distance = [0.0]
+        test_param_reward_rpy = [0.0]
+        test_param_obs_timestep = [False]
+        test_param_obs_action_difference = [True]
+        test_param_entropy_coefficent = [0]
+        test_param_reward_truncation = [-1.0]
+        test_param_clip_range = [0.3]
 
-    for learning_rate in test_param_learning_rate:
-        parameters['learning_rate'] = learning_rate
+        for learning_rate in test_param_learning_rate:
+            parameters_PPO['learning_rate'] = learning_rate
 
-        for reward_target_distance in test_param_reward_target_distance:
-            parameters['reward_target_distance'] = reward_target_distance
+            for reward_target_distance in test_param_reward_target_distance:
+                parameters_PPO['reward_target_distance'] = reward_target_distance
 
-            for reward_object_distance_delta in test_param_reward_object_distance_delta:
-                parameters['reward_object_distance_delta'] = reward_object_distance_delta
+                for reward_object_distance_delta in test_param_reward_object_distance_delta:
+                    parameters_PPO['reward_object_distance_delta'] = reward_object_distance_delta
 
-                for reward_in_target in test_param_reward_in_target:
-                    parameters['reward_in_target'] = reward_in_target
+                    for reward_in_target in test_param_reward_in_target:
+                        parameters_PPO['reward_in_target'] = reward_in_target
 
-                    for reward_object_distance in test_param_reward_object_distance:
-                        parameters['reward_object_distance'] = reward_object_distance
+                        for reward_object_distance in test_param_reward_object_distance:
+                            parameters_PPO['reward_object_distance'] = reward_object_distance
 
-                        for reward_rpy in test_param_reward_rpy:
-                            parameters['reward_rpy'] = reward_rpy
+                            for reward_rpy in test_param_reward_rpy:
+                                parameters_PPO['reward_rpy'] = reward_rpy
 
-                            for obs_timestep in test_param_obs_timestep:
-                                parameters['obs_timestep'] = obs_timestep
+                                for obs_timestep in test_param_obs_timestep:
+                                    parameters_PPO['obs_timestep'] = obs_timestep
 
-                                for reward_truncation in test_param_reward_truncation:
-                                    parameters['reward_truncation'] = reward_truncation
+                                    for reward_truncation in test_param_reward_truncation:
+                                        parameters_PPO['reward_truncation'] = reward_truncation
 
-                                    for obs_action_difference in test_param_obs_action_difference:
-                                        parameters['obs_action_difference'] = obs_action_difference
+                                        for obs_action_difference in test_param_obs_action_difference:
+                                            parameters_PPO['obs_action_difference'] = obs_action_difference
 
-                                        for entropy_coefficient in test_param_entropy_coefficent:
-                                            parameters['entropy_coefficient'] = entropy_coefficient
+                                            for entropy_coefficient in test_param_entropy_coefficent:
+                                                parameters_PPO['entropy_coefficient'] = entropy_coefficient
 
-                                            for clip_range in test_param_clip_range:
-                                                parameters['clip_range'] = clip_range
-            
-                                                PPO = Train_PPO(parameters=parameters)
+                                                for clip_range in test_param_clip_range:
+                                                    parameters_PPO['clip_range'] = clip_range
 
-                                                time_taken.append(PPO.train())
+                                                    PPO = Train_PPO(parameters=parameters_PPO)
 
+                                                    time_taken.append(PPO.train())
+
+    if rl_algorithm == "DDPG":
+        test_param_reward_object_distance = [-0.5, -1.0, -1.5]
+        test_param_nr_of_env = [4, 1]
+
+        for reward_object_distance in test_param_reward_object_distance:
+            parameters_DDPG["reward_object_distance"] = reward_object_distance
+
+            for nr_of_env in test_param_nr_of_env:
+                parameters_DDPG["nr_of_env"] = nr_of_env
+
+                DDPG = Train_DDPG(parameters = parameters_DDPG)
+
+                time_taken.append(DDPG.train())
+        
     print(time_taken)
