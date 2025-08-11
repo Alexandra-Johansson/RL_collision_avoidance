56638e9153a75646d4733110d3bbc91dfa813e40
Alexandra Johansson <alejo009@student.liu.se>
2025-08-11 08:56:46 +0200

    Fixed testing of model

--- Training/code/test_RL.py
+++ Training/code/test_RL.py
@@ -4,28 +4,28 @@ import numpy as np
 from data_handling import Txt_file
 from gym_pybullet_drones.utils.utils import sync
 from RLEnv import RLEnv
-from stable_baselines3 import PPO, DDPG
-from stable_baselines3.common.evaluation import evaluate_policy
+from stable_baselines3 import DDPG, PPO
 from stable_baselines3.common.env_util import make_vec_env
+from stable_baselines3.common.evaluation import evaluate_policy
 from stable_baselines3.common.monitor import Monitor
-from stable_baselines3.common.vec_env import SubprocVecEnv, VecNormalize, DummyVecEnv
+from stable_baselines3.common.vec_env import DummyVecEnv, SubprocVecEnv, VecNormalize
 
-folder = "Training/results/PPO_training_2025.08.08-08.21.46"  # Path to the folder with the trained model
+folder = "Training/results/PPO_training_2025.08.08-14.12.10"  # Path to the folder with the trained model
 
 param_file = Txt_file(folder)
 parameters = param_file.load_parameters()
 
-model = PPO.load(folder + "/best_model.zip")
+model = PPO.load(folder + "/final_model.zip")
 print("Model loaded")
 
 parameters['gui'] = True
-#test_env = Monitor(RLEnv(parameters = parameters))
 
-test_env = RLEnv(parameters = parameters)
-test_env = DummyVecEnv([test_env])
-test_env = VecNormalize.load(folder + "/vecnormalize.pkl", test_env)
+test_env = DummyVecEnv([lambda: RLEnv(parameters = parameters)])
+test_env = VecNormalize.load(folder + "/final_vecnormalize.pkl", test_env)
 test_env.training = False
 test_env.norm_reward = False
+
+input("Press enter...")
 
 mean_reward, std_reward = evaluate_policy(model,
                                           test_env,
