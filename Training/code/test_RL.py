2eb6d837e2ac274f773fc1fe78c3479a3934b66d
Alexandra Johansson <alejo009@student.liu.se>
2025-09-24 09:44:56 +0200

    Added recording functionality

--- Training/code/test_RL.py
+++ Training/code/test_RL.py
@@ -10,7 +10,7 @@ from stable_baselines3.common.evaluation import evaluate_policy
 from stable_baselines3.common.monitor import Monitor
 from stable_baselines3.common.vec_env import DummyVecEnv, SubprocVecEnv, VecNormalize
 
-folder = "Training/results/PPO_training_2025.08.08-14.12.10"  # Path to the folder with the trained model
+folder = "Training/results/PPO_training_2025.09.02-20.15.19"  # Path to the folder with the trained model
 
 param_file = Txt_file(folder)
 parameters = param_file.load_parameters()
@@ -19,17 +19,18 @@ model = PPO.load(folder + "/final_model.zip")
 print("Model loaded")
 
 parameters['gui'] = True
+parameters['record'] = True
 
 test_env = DummyVecEnv([lambda: RLEnv(parameters = parameters)])
 test_env = VecNormalize.load(folder + "/final_vecnormalize.pkl", test_env)
 test_env.training = False
 test_env.norm_reward = False
 
-input("Press enter...")
+#input("Press enter...")
 
 mean_reward, std_reward = evaluate_policy(model,
                                           test_env,
-                                          n_eval_episodes=20,
+                                          n_eval_episodes=5,
                                           deterministic = True)
 
 print("\n\n\nMean reward ", mean_reward, " +- ", std_reward, "\n\n")
