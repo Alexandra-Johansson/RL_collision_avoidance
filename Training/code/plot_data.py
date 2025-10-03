2fbc31ec81da1c4e5cc1c68de3dc080af939f7ba
Alexandra Johansson <alejo009@student.liu.se>
2025-10-03 13:17:06 +0200

    Fixed minor error

--- Training/code/plot_data.py
+++ Training/code/plot_data.py
@@ -3,11 +3,12 @@ from data_handling import Plot
 model_type = "PPO"  # "PPO" or "DDPG"
 
 model_filepath_mean_std = "final_models/ActionTypes"
-model_filepath_graphs = "final_models/PPO_95%"
-model_filepath1 = "final_models/PPO_95%"
-model_filepath2 = "final_models/PPO_good"
-model_filepath3 = "final_models/PPO_ok_1"
-model_filepath4 = "final_models/PPO_ok_2"
+model_filepath_graphs = "final_models/ActionTypes/PPO_PID_2"
+model_filepath1 = "final_models/ActionTypes/PPO_VEL_1"
+model_filepath2 = "final_models/ActionTypes/PPO_VEL_2"
+model_filepath3 = "final_models/ActionTypes/PPO_VEL_3"
+model_filepath4 = "final_models/ActionTypes/PPO_VEL_4"
+model_filepath5 = "final_models/ActionTypes/PPO_VEL_5"
 
 plot_mean_std = Plot(model_filepath_mean_std, model_type)
 plot_graphs = Plot(model_filepath_graphs, model_type)
@@ -15,13 +16,15 @@ plot1 = Plot(model_filepath1, model_type)
 plot2 = Plot(model_filepath2, model_type)
 plot3 = Plot(model_filepath3, model_type)
 plot4 = Plot(model_filepath4, model_type)
+plot5 = Plot(model_filepath5, model_type)
 
 #plot1.tensorboard()
 #plot2.tensorboard()
 #plot3.tensorboard()
 #plot4.tensorboard()
+#plot5.tensorboard()
 
-#plot_mean_std.plot_graphs()
+#plot_graphs.plot_graphs()
 
 file_names = ["PPO_PID", "PPO_VEL"]
 number_of_files_each = 5
