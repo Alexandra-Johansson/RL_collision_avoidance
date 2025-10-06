6f2032b2ba54b203bdf45f11d984a0a8069e579c
Alexandra Johansson <alejo009@student.liu.se>
2025-10-06 13:34:18 +0200

    Added topdown view

--- Training/code/test_RL.py
+++ Training/code/test_RL.py
@@ -9,8 +9,9 @@ from stable_baselines3.common.env_util import make_vec_env
 from stable_baselines3.common.evaluation import evaluate_policy
 from stable_baselines3.common.monitor import Monitor
 from stable_baselines3.common.vec_env import DummyVecEnv, SubprocVecEnv, VecNormalize
+import pybullet as pb
 
-folder = "Training/results/PPO_training_2025.09.02-20.15.19"  # Path to the folder with the trained model
+folder = "final_models/transformations/PPO_transformation_true_7"  # Path to the folder with the trained model
 
 param_file = Txt_file(folder)
 parameters = param_file.load_parameters()
@@ -26,11 +27,21 @@ test_env = VecNormalize.load(folder + "/final_vecnormalize.pkl", test_env)
 test_env.training = False
 test_env.norm_reward = False
 
+
+# Top-down view of the simulation
+pb.resetDebugVisualizerCamera(
+    cameraDistance=3.0,     # Distance from the target
+    cameraYaw=30,           # Horizontal angle (left/right)
+    cameraPitch=-90,        # Vertical angle (up/down)
+    cameraTargetPosition=[0, 0, 1]  # Where the camera is looking
+)
+
+
 #input("Press enter...")
 
 mean_reward, std_reward = evaluate_policy(model,
                                           test_env,
-                                          n_eval_episodes=5,
+                                          n_eval_episodes=10,
                                           deterministic = True)
 
 print("\n\n\nMean reward ", mean_reward, " +- ", std_reward, "\n\n")
